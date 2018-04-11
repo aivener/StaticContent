16e31ef19d354b4f2ed1b1efe2c0c51c21965e86
Ryan Allen <rallen090@gmail.com>
2018-04-11 13:47:25 -0400

    Create names.js

---
+++ names.js
@@ -0,0 +1,16 @@
+var usernamesByCategeory = {};
+usernamesByCategeory['fi'] = ["jreagan", "kpeng", "dkang", "agallagher", "nmorrow", "dcalabrese", "nsidawy"];
+usernamesByCategeory['mrc'] = ["dtiedemann", "jkim", "prege", "asheeler", "rallen", "yqu"];
+usernamesByCategeory['react-big'] = ["jreagan", "dtiedemann", "jkim", "kpeng", "dkang", "prege", "asheeler", "agallagher", "rallen", "nmorrow", "dcalabrese", "yqu"];
+usernamesByCategeory['react-small'] = ["jreagan", "jkim", "kpeng", "dkang", "agallagher", "nmorrow", "yqu"];
+usernamesByCategeory['web-big'] = ["jreagan", "dtiedemann", "jkim", "kpeng", "dkang", "prege", "aivener", "asheeler", "agallagher", "rallen", "nmorrow", "dcalabrese", "yqu"];
+usernamesByCategeory['web-small'] = ["jreagan", "jkim", "dkang", "asheeler", "rallen", "yqu"];
+usernamesByCategeory['results-large'] = ["dtiedemann", "jkim", "kpeng", "dkang", "prege", "aivener", "asheeler", "rallen", "dcalabrese", "pwang", "yqu"];
+usernamesByCategeory['results-small'] = ["dtiedemann", "jkim", "kpeng", "prege", "asheeler", "rallen", "yqu"];
+usernamesByCategeory['du-large'] = ["dtiedemann", "jkim", "kpeng", "prege", "aivener", "asheeler", "rallen", "dcalabrese", "yqu"];
+usernamesByCategeory['du-small'] = ["dtiedemann", "prege", "aivener", "rallen", "yqu"];
+usernamesByCategeory['calc-large'] = ["dtiedemann", "jkim", "kpeng", "dkang", "prege", "aivener", "asheeler", "rallen", "dcalabrese", "pwang", "yqu"];
+usernamesByCategeory['calc-small'] = ["dtiedemann", "kpeng", "prege", "aivener", "rallen", "pwang", "yqu"];
+usernamesByCategeory['tacalc-large'] = ["jkim", "kpeng", "dkang", "aivener", "asheeler", "agallagher", "rallen", "dcalabrese", "pwang", "yqu", "nsidawy"];
+usernamesByCategeory['tacalc-small'] = ["jkim", "aivener", "agallagher", "rallen", "dcalabrese", "pwang", "yqu", "nsidawy"];
+usernamesByCategeory['azure'] = ["prege", "agallagher", "dcalabrese", "pwang", "nsidawy"];
