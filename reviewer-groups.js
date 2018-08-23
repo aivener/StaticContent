7a37517d04f30b7d8b60333db085e26af5afb847
Ryan Allen <rallen090@gmail.com>
2018-08-23 17:40:16 -0400

    Create reviewer-groups.js

---
+++ reviewer-groups.js
@@ -0,0 +1,5 @@
+window.usernamesByGroup= {};
+window.usernamesByGroup['mcipropel'] = ["jmerali", "jkim", "kpeng", "rallen", "nmorrow", "hpark"];
+window.usernamesByGroup['mciweb'] = ["jreagan", "yqu", "afreix", "cwalters", "lchen", "fbalashov", "rstpierre", "samjones"];
+window.usernamesByGroup['mcianalytics'] = ["dtiedemann", "prege", "cstout", "aivener", "asheeler", "yyao"];
+window.usernamesByGroup['mcimdp'] = [ "agallagher", "dcalabrese", "pwang", "nsidawy", "jmalin", "kchin"];
