b0e51f326f1211083d1f97c09079efebf38da3ee
Ryan Allen <rallen090@gmail.com>
2018-09-13 13:29:10 -0400

    Update reviewer-groups.js

--- reviewer-groups.js
+++ reviewer-groups.js
@@ -1,6 +1,6 @@
 // Add a usernamesByGroup key along with usernames to create a group
 window.usernamesByGroup= {};
 window.usernamesByGroup['mci-propel'] = ["jmerali", "jkim", "kpeng", "rallen", "nmorrow", "hpark"];
-window.usernamesByGroup['mci-web'] = ["jreagan", "yqu", "afreix", "cwalters", "lchen", "fbalashov", "rstpierre", "samjones"];
+window.usernamesByGroup['mci-web'] = ["jreagan", "yqu", "afreix", "cwalters", "lchen", "fbalashov", "rstpierre"];
 window.usernamesByGroup['mci-analytics'] = ["dtiedemann", "prege", "cstout", "aivener", "asheeler", "yyao"];
 window.usernamesByGroup['mci-mdp'] = [ "agallagher", "dcalabrese", "pwang", "nsidawy", "jmalin", "kchin"];
