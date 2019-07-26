c084b522d54b2efa8f06ad6e08bd9bd34041e115
Allie Ivener <allison.ivener@gmail.com>
2019-07-26 09:32:54 -0700

    add tim to gemini group

--- reviewer-groups.js
+++ reviewer-groups.js
@@ -4,4 +4,4 @@ window.usernamesByGroup['mci-propel'] = ["jmerali", "jkim", "kpeng", "rallen", "
 window.usernamesByGroup['mci-web'] = ["jreagan", "yqu", "afreix", "fbalashov", "rstpierre"];
 window.usernamesByGroup['mci-analytics'] = ["dtiedemann", "prege", "cstout", "aivener", "asheeler", "yyao"];
 window.usernamesByGroup['mci-mdp'] = [ "agallagher", "pwang", "nsidawy", "jmalin", "kchin"];
-window.usernamesByGroup['mci-gemini'] = [ "aivener", "jreagan", "cstout", "asheeler", "fbalashov", "rstpierre", "yyao", "yqu"];
+window.usernamesByGroup['mci-gemini'] = [ "aivener", "jreagan", "cstout", "asheeler", "fbalashov", "rstpierre", "yyao", "yqu", "tgieseking"];
