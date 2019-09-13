385a69b18abd07be4c2c52fcbe4932480e75131e
Allie Ivener <aivener@predictivetechnologies.com>
2019-09-13 10:23:24 -0700

    create hudson and potomac groups

--- reviewer-groups.js
+++ reviewer-groups.js
@@ -1,7 +1,7 @@
 // Add a usernamesByGroup key along with usernames to create a group
 window.usernamesByGroup= {};
 window.usernamesByGroup['mci-propel'] = ["jmerali", "jkim", "kpeng", "rallen", "nmorrow", "hpark"];
-window.usernamesByGroup['mci-web'] = ["jreagan", "yqu", "afreix", "fbalashov", "rstpierre"];
-window.usernamesByGroup['mci-analytics'] = ["dtiedemann", "prege", "cstout", "aivener", "asheeler", "yyao"];
 window.usernamesByGroup['mci-mdp'] = [ "agallagher", "pwang", "nsidawy", "jmalin", "kchin"];
 window.usernamesByGroup['mci-gemini'] = [ "aivener", "jreagan", "cstout", "fbalashov", "rstpierre", "yyao", "yqu", "tgieseking", "apeltz", "arapoport", "doconnell", "bzelditch"];
+window.usernamesByGroup['mci-hudson'] = [ "aivener", "fbalashov", "rstpierre", "yyao", "apeltz", "bzelditch"];
+window.usernamesByGroup['mci-potomac'] = [ "jreagan", "cstout", "yqu", "tgieseking", "arapoport", "doconnell""];
