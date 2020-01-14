e735b4c30678f6de94580dac62580f22ba7bfc56
Allie Ivener <aivener@predictivetechnologies.com>
2020-01-14 15:35:29 -0800

    add Sid to Hudson

--- reviewer-groups.js
+++ reviewer-groups.js
@@ -2,6 +2,6 @@
 window.usernamesByGroup= {};
 window.usernamesByGroup['mci-propel'] = ["jmerali", "jkim", "kpeng", "rallen", "nmorrow", "hpark"];
 window.usernamesByGroup['mci-mdp'] = [ "agallagher", "pwang", "nsidawy", "jmalin", "kchin"];
-window.usernamesByGroup['mci-gemini'] = [ "aivener", "jreagan", "cstout", "fbalashov", "rstpierre", "yyao", "yqu", "tgieseking", "apeltz", "arapoport", "doconnell", "bzelditch"];
-window.usernamesByGroup['mci-hudson'] = [ "aivener", "fbalashov", "rstpierre", "yyao", "apeltz", "bzelditch", "yqu"];
+window.usernamesByGroup['mci-gemini'] = [ "aivener", "jreagan", "cstout", "fbalashov", "rstpierre", "yyao", "yqu", "tgieseking", "apeltz", "arapoport", "doconnell", "bzelditch", "sanand"];
+window.usernamesByGroup['mci-hudson'] = [ "aivener", "fbalashov", "rstpierre", "yyao", "apeltz", "bzelditch", "yqu", "sanand"];
 window.usernamesByGroup['mci-potomac'] = [ "jreagan", "cstout", "yqu", "tgieseking", "arapoport", "doconnell", "yyao"];
